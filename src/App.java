9fa6da8362ebd664dc0c5a7327d4201c0af792c4
ellsonmendesyt <elsonmendesoficial@gmail.com>
2021-11-18 08:06:50 -0300

    primeiro commit

---
+++ src/App.java
@@ -0,0 +1,21 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        // System.out.println("Hello, World!");
+
+
+        int a=5;
+        char b='i';
+        long c= 400;
+        double d = 3.4;
+
+        String nome = "elson";
+        Cat cat = new Cat();
+        System.out.println(cat.speak());
+
+       
+
+
+
+        System.out.println("nome: "+nome.toUpperCase());
+    }
+}
---
+++ src/Cat.java
@@ -0,0 +1,5 @@
+public class Cat {
+    public static String speak(){
+        return "Hello I'm a dog!";
+    }
+}
